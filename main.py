18c067a5cd1c5b1fa8d4230b8d667047b18cdbef
Snare-Hawk <85657152+Snare-Hawk@users.noreply.github.com>
2024-05-25 01:55:08 -0400

    why wasn't i using dictionaries before?

--- main.py
+++ main.py
@@ -7,41 +7,28 @@ from tkinter import *
 from io import BytesIO
 from PIL import Image, ImageTk
 
-
-URL = "https://pokemondb.net/static/sitemaps/pokemondb.xml"
-page = requests.get(URL)
-
-reggie = "https\\:\\/\\/pokemondb\\.net\\/pokedex\\/[^(game|stats)].*"
-
-# this gets the XML file
-data = page.content
-
-dataDict = xmltodict.parse(data)
-masterList = []
-
-for i in dataDict['urlset']['url']:
-    if ('priority' in i):
-        if re.search(reggie, i.get('loc')):
-            masterList.append(i)
-
-# for pkmn in masterList
-newURL = masterList[0]['loc']
-newPage = requests.get(newURL)
-
-# get info from pkmn page
-soup = bs(newPage.content, "html.parser")
-pkmnName = soup.find("h1").text
-pkmnImg = soup.find_all("img")
-
-
-# json_object = json.dumps(pkmnImg, indent=4)
-
-# with open("pkmn.json", "w+") as outfile:
-# 	outfile.write(json_object)
-
 root = Tk()
-root.title(pkmnName)
-root.geometry("500x500")
+
+def getName(url):
+    newPage = requests.get(url)
+    soup = bs(newPage.content, "html.parser")
+    
+    # get info from pkmn page
+    pkmnName = soup.find("h1").text
+
+    return pkmnName
+
+def getImgLink(url):
+    newPage = requests.get(url)
+    soup = bs(newPage.content, "html.parser")
+
+    # gets all images on the page and puts them into an array
+    pkmnImg = soup.find_all("img")
+
+    # we only need pkmnImg[1], at least for now
+    pkmnImgLink = pkmnImg[1]['src']
+
+    return pkmnImgLink
 
 def urlToImage(url):
     response = requests.get(url)
@@ -51,26 +38,61 @@ def urlToImage(url):
     tk_img = ImageTk.PhotoImage(img)
     return tk_img
 
-pkmnImg = urlToImage(pkmnImg[1]['src'])
-Label(root, text="hi", font=("Ubuntu", 24)).pack()
-Label(root,image=pkmnImg).pack()
+URL = "https://pokemondb.net/static/sitemaps/pokemondb.xml"
+page = requests.get(URL)
 
-root.mainloop()
+reggie = "https\\:\\/\\/pokemondb\\.net\\/pokedex\\/[^(game|stats)].*"
 
-# ws = Tk()  
-# ws.title('PythonGuides')
-# ws.geometry('500x500')
+# this gets the XML file
+data = page.content
 
-# canvas = Canvas(ws, width=4000, height=4000)  
-# canvas.pack()  
-# img = ImageTk.PhotoImage(Image.open())  
-# canvas.create_image(40, 20, anchor=NW, image=img) 
-# ws.mainloop() 
+# make it a dict
+dataDict = xmltodict.parse(data)
+
+# initialize pkmnPageList to hold all the links
+pkmnPageList = []
+
+for i in dataDict['urlset']['url']:
+    if ('priority' in i):
+        if re.search(reggie, i.get('loc')):
+            pkmnPageList.append(i)
+
+# array of tkinter images
+masterPkmnImgs = []
+
+arrayOfDicts = []
+
+for pkmnPage in pkmnPageList: 
+    # gets the link to the pokemon's page 
+    newURL = pkmnPage['loc']
+
+    dictItem = {"name":getName(newURL), "img":getImgLink(newURL)}
+    arrayOfDicts.append(dictItem)
 
 
+# json_object = json.dumps(pkmnImg, indent=4)
+
+# with open("pkmn.json", "w+") as outfile:
+# 	outfile.write(json_object)
+
+# root.title(pkmnName)
+# root.geometry("500x500")
+
+# Label(root, text="hi", font=("Ubuntu", 24)).pack()
+
+"""
+for i in masterPkmnImgs:
+    # pkmnImg = urlToImage(pkmnImg[1]['src'])
+    label = Label(root, image=i)
+    label.pack()
+"""
+# for names, imgs in zip()
+# root.mainloop()
 
 # written to a json for easier viewing
-json_object = json.dumps(masterList, indent=4)
+json_object = json.dumps(arrayOfDicts, indent=4)
 
-with open("jobs.json", "w+") as outfile:
+with open("PKMNinfo.json", "w+") as outfile:
 	outfile.write(json_object)
+
+print("Done!")
